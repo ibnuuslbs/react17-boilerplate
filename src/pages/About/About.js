30fa6beccd6fa4da2f18ef0d3bee190f19a413ae
Dindaragil <dindawardani36@gmail.com>
2021-04-27 14:05:20 +0700

    create about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {PageHeader} from "antd";
+import logo from "../../img/logocoda.png";
 
 export const About = () => {
     return <div>
@@ -13,6 +14,20 @@ export const About = () => {
             title={"About"}
         >
         </PageHeader>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut recusandae velit! Consequatur corporis, eum fuga, harum incidunt laboriosam minus necessitatibus neque non nostrum pariatur tempore. Dignissimos impedit rem tempora!
+        
+        <div id="aboutcard"className="mb-3" style={{minWidth: '120%', boxShadow: '0px 5px 20px  rgba(0,0,0,0.2)', backgroundColor: '#e3e8ee'}}>
+  <div className="row g-0"> 
+    <div className="col-md-4">
+      <img src={logo} alt="..." width="300"/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body" style={{ paddingTop: '10%', paddingRight: '10%'}}>
+        <h5 className="card-title">Coda.com</h5>
+        <p className="card-text">Coda.com is a safe and reliable place to buy and sell musical instruments because we only provide the best shops for you. Enjoy shopping without having to be afraid of the authenticity and quality of the goods on our platform.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 };
